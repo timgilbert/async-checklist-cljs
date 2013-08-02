39b9b105cb7cc263aebbc26168be48db7e8a657c
Tim Gilbert <timothy_l.gilbert@nytimes.com>
2013-08-02 14:44:22 -0400

    Creating new project based on cljs-kickoff

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject async-checklist-cljs "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ring "1.1.8"]]
+  :plugins [[lein-cljsbuild "0.3.0"]
+            [lein-ring "0.8.3"]]
+  :hooks [leiningen.cljsbuild]
+  :source-paths ["src/clj"]
+  :cljsbuild { 
+    :builds {
+      :main {
+        :source-paths ["src/cljs"]
+        :compiler {:output-to "resources/public/js/cljs.js"
+                   :optimizations :simple
+                   :pretty-print true}
+        :jar true}}}
+  :main async-checklist-cljs.server
+  :ring {:handler async-checklist-cljs.server/app})
+
