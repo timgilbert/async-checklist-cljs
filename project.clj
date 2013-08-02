b3e585e73d2a8b77df89ef90e84a0bc6b51b4a0e
Tim Gilbert <timothy_l.gilbert@nytimes.com>
2013-08-02 15:26:36 -0400

    Updating ring to just serve static files

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
 (defproject async-checklist-cljs "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring "1.1.8"]]
-  :plugins [[lein-cljsbuild "0.3.0"]
-            [lein-ring "0.8.3"]]
+  :description "TODO app written in clojurescript with core.async"
+  :url "http://github.com/timgilbert"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring "1.2.0"]]
+  :plugins [[lein-cljsbuild "0.3.2"]
+            [lein-ring "0.8.6"]
+            [io.rkn/core.async "0.1.0-SNAPSHOT"]]
   :hooks [leiningen.cljsbuild]
   :source-paths ["src/clj"]
-  :cljsbuild { 
+  :cljsbuild {
     :builds {
       :main {
         :source-paths ["src/cljs"]
